aea18d3670f16aa50e772cf8cd8ebf24abdb8642
Andry1910 <100972786+Andry1910@users.noreply.github.com>
2024-05-19 21:33:57 -0300

    sign up finalizado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from '../pages/login';
 import { HomePage } from '../pages/home';
+import SignUpPage from '../pages/sign-up';
 
 export function MyRoutes() {
     return (
@@ -8,6 +9,7 @@ export function MyRoutes() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/home" element={<HomePage />} />
+                <Route path="/cadastrar-clinica" element={<SignUpPage />} />
             </Routes>
         </BrowserRouter>
     );
